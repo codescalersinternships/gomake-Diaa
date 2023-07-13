fb432f71ceb93b59832120e762002be5b5458ece
diaabadr <diaabadr82@gmail.com>
2023-07-13 17:05:35 +0300

    removing workflow file

--- internal/makefile_test.go
+++ internal/makefile_test.go
@@ -3,6 +3,7 @@ package makefile
 import (
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,3 +46,66 @@ func TestReadMakefile(t *testing.T) {
 		})
 	}
 }
+
+// func TestParseMakefile(t *testing.T) {
+// 	t.Parallel()
+
+// 	testCases := []struct {
+// 		name           string
+// 		fileContent    string
+// 		expectedError  error
+// 		expectedAdjList Graph
+// 		expectedTargsCmds CommandMap
+// 		failureMessage string
+// 	}{
+// 		{
+// 			name: "Valid format",
+// 			fileContent: `run: build
+// 	echo run
+// # comment
+// build:
+// 	echo build`,
+// 			expectedError:  nil,
+// 			expectedAdjList: Graph{
+// 				"run":[]string{"build"},
+// 				"build":[] string{},
+// 			},
+// 			expectedTargsCmds: CommandMap{
+// 				"run":"echo run",
+// 				"build":"echo build",
+// 			},
+// 			failureMessage: "got error while it's a valid format",
+// 		},
+// 		{
+// 			name: "invalid format because of global command",
+// 			fileContent: `
+// echo test
+// run:
+// 	echo run`,
+// 			expectedError:  ErrInvalidFormat,
+// 			expectedAdjList: nil,
+// 			expectedTargsCmds: nil,
+// 			failureMessage: "failed to detect global command",
+// 		},
+// 		{
+// 			name: "invalid format because of \t before target",
+// 			fileContent: `
+// 	run:
+// 	echo run`,
+// 			expectedError:  ErrInvalidFormat,
+// 			expectedAdjList: nil,
+// 			expectedTargsCmds: nil,
+// 			failureMessage: "failed to detect \t before target",
+// 		},
+// 	}
+
+// 	for _,tc := range testCases{
+// 		t.Run(tc.name,func(t *testing.T) {
+// 			reader := strings.NewReader(tc.fileContent)
+// 			gotAdjList,gotTargsCmds,err:= ParseMakefile(reader)
+
+// 			assert.
+// 		})
+// 	}
+
+// }
